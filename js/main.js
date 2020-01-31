7e5410c607fc5440cb9a143d22718cffac865f5f
Andrea Bottini <andrea.bottini22@gmail.com>
2020-01-31 16:14:23 +0100

    Chiamata Api: funziona

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+// Attraverso una chiamata ajax all’Api di Boolean avremo a disposizione una decina di dischi musicali.
+// Servendoci di handlebars stampiamo tutto a schermo.
+
+
 $(document).ready(function() {
-	//Code
+	$.ajax( {
+    url: "https://flynn.boolean.careers/exercises/api/array/music",
+    method: "GET",
+    success: function (data, response) {
+      console.log(data.response);
+    },
+    error: function (data, response) {
+      alert('Errore del server. Correggi!')
+    }
+
+  })
 });
